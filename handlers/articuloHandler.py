24115a26f486e7c7005a73c5478370db2e10229c
AngelDPS <adps@gmail.com>
2023-08-16 14:02:00 -0400

    FIX 16-08-2023

--- handlers/articuloHandler.py
+++ handlers/articuloHandler.py
@@ -46,7 +46,7 @@ class TipoPublicacion(Enum):
 
 
 class ArticuloHandler(ItemHandler):
-    item = "articulo"
+    ITEM_TYPE = "articulo"
 
     def __init__(self, evento):
         """Constructor de la clase
